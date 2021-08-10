1a85488be48137a372285bc531d01738b200001d
sulymon51 <sulymon51@gmail.com>
2021-08-10 13:46:17 +0100

    upload page

---
+++ build/precache-manifest.c24ffda11534822dab04919c09459338.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0393f8cef3d07a10b01",
+    "url": "/static/css/main.c86e6592.chunk.css"
+  },
+  {
+    "revision": "f0393f8cef3d07a10b01",
+    "url": "/static/js/main.f0393f8c.chunk.js"
+  },
+  {
+    "revision": "82209a8f145e995de44d",
+    "url": "/static/css/1.02fcf623.chunk.css"
+  },
+  {
+    "revision": "82209a8f145e995de44d",
+    "url": "/static/js/1.82209a8f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50116c912a38a9da8409ca071df23848",
+    "url": "/static/media/item3.50116c91.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "4a4612d929d00dd61da9a27acc5c89a8",
+    "url": "/static/media/item5.4a4612d9.jpg"
+  },
+  {
+    "revision": "2630abd1d97ee11899ef13e40d7de56b",
+    "url": "/index.html"
+  }
+];
